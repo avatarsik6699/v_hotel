e863977296d13e53f338cb81f477c858b7de6077
Vlad Godlevskii <avatarsik6699@gmail.com>
2021-08-19 20:04:46 +0300

    add button

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import './style.scss';
 import './shared/views/Dropdown/index';
-import { RangeSlider } from 'shared/views/RangeSlider';
 
-const rangeSlider = new RangeSlider('.slider-wrapper', {
-  connect: true,
-  start: [5000, 10000],
-  range: {
-    min: 5000,
-    max: 10000,
-  },
-});
+// const rangeSlider = new RangeSlider('.slider-wrapper', {
+//   connect: true,
+//   start: [5000, 10000],
+//   range: {
+//     min: 5000,
+//     max: 10000,
+//   },
+// });
